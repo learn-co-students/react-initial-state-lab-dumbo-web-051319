e595fd417edbc62b90d7a0e88e1b92a229febb5f
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-07-16 01:13:22 -0400

    used state

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,23 @@
-// your Bomb code here!
+import React, { Component } from 'react';
+
+export default class Bomb extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      secondsLeft: this.props.initialCount
+    }
+  }
+
+
+  render () {
+    let result = this.state.secondsLeft === 0? "Boom!" : `${this.state.secondsLeft} seconds left before I go boom!`;
+    return (
+      <div>{result}</div>
+    )
+  }
+
+}
+
+// check to see if the secondsLeft is 0,
+// if it isnt print out message with secondsLeft value
+// if it is, Boom!
